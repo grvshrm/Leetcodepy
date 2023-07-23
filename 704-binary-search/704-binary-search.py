3be5d8b77470975542830f43396906cf0be65792
grvshrm <49127305+grvshrm@users.noreply.github.com>
2023-07-24 00:19:17 +0530

    Time: 256 ms (19.36%), Space: 17.9 MB (70.78%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -3,7 +3,7 @@ class Solution:
         start = 0
         end = len(nums) - 1
         while(start <= end):
-            mid = start + (end - start) // 2
+            mid = (start + end) // 2
             if(nums[mid] == target):
                 return mid
             elif(target > nums[mid]):
