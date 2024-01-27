86e81f8c767a2364004d8e6e4c413ff433c91f7e
grvshrm <49127305+grvshrm@users.noreply.github.com>
2024-01-27 17:23:37 +0530

    Time: 45 ms (65.06%), Space: 17.2 MB (95.46%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -12,15 +12,17 @@ class Solution:
         while levelQ:
             left_indx, right_indx = 0, 0
             size = len(levelQ)
+            curMin = levelQ[0][1]
             for i in range(size):
-                cur, cur_indx = levelQ.pop(0)
+                cur, indx = levelQ.pop(0)
+                cur_id = indx - curMin
                 if i == 0:
-                    left_indx = cur_indx
+                    left_indx = cur_id
                 if i == size - 1:
-                    right_indx = cur_indx
+                    right_indx = cur_id
                 if cur.left:
-                    levelQ.append([cur.left, 2 * cur_indx + 1])
+                    levelQ.append([cur.left, 2 * cur_id + 1])
                 if cur.right:
-                    levelQ.append([cur.right, 2 * cur_indx + 2])
+                    levelQ.append([cur.right, 2 * cur_id + 2])
             ans = max(ans, right_indx - left_indx + 1)
         return ans
