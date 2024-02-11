ebddb90f018c3868372993aa360551c4f5221792
grvshrm <49127305+grvshrm@users.noreply.github.com>
2024-02-11 13:48:22 +0530

    Time: 1262 ms (32.14%), Space: 23.7 MB (95.98%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution: 
     def sortArray(self, nums: List[int]) -> List[int]:
         low, high = 0, len(nums) - 1
         self.mergeSort(nums, low, high)
@@ -30,3 +30,26 @@ class Solution:
             right += 1
         for i in range(low, high+1):
             arr[i] = temp[i - low]
+
+    """def sortArray(self, nums: List[int]) -> List[int]:
+        self.quickSort(nums, 0, len(nums) - 1)
+        return nums
+        
+    def partition(self, arr: list, low: int, high: int):
+        pivot = arr[low]
+        i, j = low, high
+        while i < j:
+            while i <= high-1 and arr[i] <= pivot:
+                i += 1
+            while j >= low+1 and arr[j] > pivot:
+                j -= 1
+            if i < j:
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[j], arr[low] = arr[low], arr[j]
+        return j
+
+    def quickSort(self, arr: list, low: int, high: int):
+        if low < high:
+            pIndex = self.partition(arr, low, high)
+            self.quickSort(arr, low, pIndex - 1)
+            self.quickSort(arr, pIndex + 1, high)"""
