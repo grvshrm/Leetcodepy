c1055b379930dd1340f5f76994e906efae7e9017
grvshrm <49127305+grvshrm@users.noreply.github.com>
2024-04-05 01:19:19 +0530

    Time: 34 ms (91.83%), Space: 16.7 MB (36.77%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -3,13 +3,13 @@ class Solution:
         n = len(nums)
         l, r = 0, n-1
         ans = []
-        def permuteUtil(nums, l, r):
-            if l == r:
+        def permuteUtil(nums, l):
+            if l == n-1:
                 ans.append(nums[:])
                 return
-            for i in range(l, r+1):
+            for i in range(l, n):
                 nums[l], nums[i] = nums[i], nums[l]
-                permuteUtil(nums, l+1, r)
+                permuteUtil(nums, l+1)
                 nums[l], nums[i] = nums[i], nums[l]
-        permuteUtil(nums, l, r)
+        permuteUtil(nums, l)
         return ans
