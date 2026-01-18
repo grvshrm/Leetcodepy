54d6a7af61bf64896eaecf4f22877b2f2c804810
grvshrm <49127305+grvshrm@users.noreply.github.com>
2026-01-18 16:26:45 +0530

    Delete 0268-missing-number directory

--- 0268-missing-number/0268-missing-number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums) + 1
-        missing_num = 0
-        for num in nums:
-            missing_num ^= num
-        for num in  range(n):
-            missing_num ^= num
-        return missing_num
