f249ee7dbc2eff20958ecac8b8799836351d507e
grvshrm <49127305+grvshrm@users.noreply.github.com>
2026-01-24 20:54:48 +0530

    Time: 12 ms (34.53%) | Memory: 21.8 MB (6.99%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ind_nums = defaultdict(list)
+        for i in range(len(nums)):
+            ind_nums[nums[i]].append(i)
+
+        print(ind_nums)
+        sorted_nums = list(sorted(nums))
+        i, j = 0, len(nums)-1
+        ans = list()
+        while i < j:
+            curr_sum = sorted_nums[i]+sorted_nums[j]
+            if curr_sum == target:
+                ans = [ind_nums[sorted_nums[i]][0], ind_nums[sorted_nums[j]][0]] if sorted_nums[i] != sorted_nums[j] else [ind_nums[sorted_nums[i]][0], ind_nums[sorted_nums[i]][1]]
+                break
+            elif curr_sum < target:
+                i+=1
+            else:
+                j-=1
+        return ans
