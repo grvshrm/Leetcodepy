d34bb61bd1876533646c3d4d1a9f87f2a1403bd4
grvshrm <49127305+grvshrm@users.noreply.github.com>
2026-05-31 22:35:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.12%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -4,10 +4,7 @@ class Solution:
         dp = [-1] * (n)
         dp[0] = nums[0]
         for i in range(1, n):
-            pick = nums[i]
-            if i >= 2:
-                pick += dp[i-2]
+            pick = (dp[i-2] if i >= 2 else 0) + nums[i]
             not_pick = dp[i-1]
             dp[i] = max(pick, not_pick)
         return dp[-1]
-
