ccf8a34d311fc56c7ebd51558d0468189e4539b3
grvshrm <49127305+grvshrm@users.noreply.github.com>
2026-05-31 22:57:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (68.53%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -5,17 +5,14 @@ class Solution:
             return 0
         if n == 1:
             return nums[0]
-        def rob_util(arr):
-            n = len(arr)
-            dp = [-1] * (n)
-            dp[0] = arr[0]
-            for i in range(1, n):
-                pick = arr[i]
-                if i >= 2:
-                    pick += dp[i-2]
-                not_pick = dp[i-1]
-                dp[i] = max(pick, not_pick)
-            return dp[-1]
-        ans1 = rob_util(nums[1:])
-        ans2 = rob_util(nums[:-1])
-        return max(ans1, ans2)
+        return max(self.rob_util(nums[1:]), self.rob_util(nums[:-1]))
+
+    def rob_util(self, nums):
+        n = len(nums)
+        dp = [-1] * n
+        dp[0] = nums[0]
+        for i in range(1, n):
+            pick = nums[i] + (dp[i-2] if i >= 2 else 0)
+            not_pick = dp[i-1]
+            dp[i] = max(pick, not_pick)
+        return dp[-1]
