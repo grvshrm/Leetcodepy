7c06b0dc44d7e7da3f935c4765c13fd49d114cbe
grvshrm <49127305+grvshrm@users.noreply.github.com>
2026-07-05 17:12:27 +0530

    Time: 6 ms (57.87%) | Memory: 20.1 MB (65.88%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -10,3 +10,16 @@ class Solution:
             r = farthest
             jumps += 1
         return jumps
+
+# class Solution:
+#     def jump(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         min_jumps = 0
+#         curr_end = 0
+#         farthest = 0
+#         for i in range(n-1):
+#             farthest = max(farthest, i + nums[i])
+#             if i == curr_end:
+#                 min_jumps += 1
+#                 curr_end = farthest
+#         return min_jumps
